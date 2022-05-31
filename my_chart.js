11f0bdd0b762a083c696af851bb8b235f6611ca9
dpliao <dunpeiliao@gmail.com>
2022-05-31 11:32:35 +0800

    add chart line

--- my_chart.js
+++ my_chart.js
@@ -1,3 +1,7 @@
+function getRandom(min, max){
+    return Math.floor(Math.random()*(max-min+1))+min;
+};
+
 const ctx = document.getElementById('myChart').getContext('2d');
 // const labels = Utils.months({count: 7});
 // const data = {
@@ -12,10 +16,11 @@ const ctx = document.getElementById('myChart').getContext('2d');
 // };
 
 var n = [];
-var arr = [];
-for(i=1;i<=365*2;i++){
+var arr = {'TSMC':[], 'ASML':[]};
+for(i=1;i<=200*2;i++){
     n.push(i);
-    arr.push(Math.floor(Math.random()*(100-50+1))+50);
+    arr['TSMC'].push(getRandom(50, 100));
+    arr['ASML'].push(getRandom(50, 100));
 };
 
 const myChart = new Chart(ctx, {
@@ -23,23 +28,44 @@ const myChart = new Chart(ctx, {
     data: {
         labels: n,
         datasets: [{
-            label: '# of search',
-            data: arr,
+            label: 'TSMC',
+            data: arr['TSMC'],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)'//,
+                // 'rgba(54, 162, 235, 0.2)',
+                // 'rgba(255, 206, 86, 0.2)',
+                // 'rgba(75, 192, 192, 0.2)',
+                // 'rgba(153, 102, 255, 0.2)',
+                // 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 1)'//,
+                // 'rgba(54, 162, 235, 1)',
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                // 'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },
+    {
+            label: 'ASML',
+            data: arr['ASML'],
+            backgroundColor: [
+                // 'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'//,
+                // 'rgba(255, 206, 86, 0.2)',
+                // 'rgba(75, 192, 192, 0.2)',
+                // 'rgba(153, 102, 255, 0.2)',
+                // 'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                // 'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'//,
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                // 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
@@ -47,7 +73,12 @@ const myChart = new Chart(ctx, {
     options: {
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: false
+            }
+        },
+        elements: {
+            point:{
+                radius: 1
             }
         }
     }
